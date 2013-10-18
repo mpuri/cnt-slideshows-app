1f51cc708bb7b621886a97d56ac764ff4e99518c
Thorsten Lorenz <thlorenz@gmx.de>
2013-10-18 15:27:09 -0400

    implementing and testing gallery trim

---
+++ app/api/lib/trim-gallery.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function trimItem(item) {
+  item.photoId = void 0;
+
+  var photo = item.photo;
+  if (!photo) return;
+
+  photo.id = void 0;
+  photo.createdAt = void 0;
+  photo.modifiedAt = void 0;
+}
+
+/**
+ * Takes in data provided by cnt-services and trims it to only retain data that the slideshow actually needs
+ * 
+ * @function trimGallery
+ * @param gallery {Object} provided by cnt-services
+ */
+var go = module.exports = function (gallery) {
+  // Setting all unneeded props to void 0 since that is more than 10x faster than deleting it
+  // http://jsperf.com/delete-vs-undefined-vs-null/22
+
+  gallery.publishHistory = void 0;
+  gallery.id = void 0;
+  gallery.photos = void 0;
+
+  var items = gallery.items;
+  if (items) items.forEach(trimItem);
+   
+  return gallery;
+};
