d88ffe656b72aec213877e3c1f2d50ad817e3d63
Joe Williams <joewillnyc@gmail.com>
2013-10-21 12:12:08 -0400

    new json to represent multiple image renditions

--- app/api/routes/slideshows.js
+++ app/api/routes/slideshows.js
@@ -4,7 +4,9 @@ var prepareSlideshow = require('../lib/prepare-slideshow');
 
 function cntServicesGallery(slug) {
   // TODO: actual hit cnt-services instead of using our mock data
-  var o = require('../test/fixtures/women-slug.json');
+  var o = slug === 'multi' 
+    ? require('../test/fixtures/women-slug-multi-renditions.json') 
+    : require('../test/fixtures/women-slug.json');
 
   // ensure we get new object each time and don't mess with original
   return JSON.parse(JSON.stringify(o));
